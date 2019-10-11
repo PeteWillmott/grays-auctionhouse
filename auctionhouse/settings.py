0a1a40a0be51b01492c63240b336c3e0c7093375
PeteWillmott <bastior@yahoo.co.uk>
2019-10-11 10:25:19 +0200

    Fixed boto deployment issue.

--- auctionhouse/settings.py
+++ auctionhouse/settings.py
@@ -148,7 +148,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
